82df6b549003297d9e7c5e5589bfd2e0c4b66917
TALITA ANTUNES <127903101+Talitaav@users.noreply.github.com>
2024-04-24 19:54:00 -0300

    Update Exercicio05.java

--- tarefa13/Exercicio05.java
+++ tarefa13/Exercicio05.java
@@ -3,18 +3,19 @@ package tarefa13;
 import java.util.Scanner;
 
 public class Exercicio05 {
+	/*
+		 * 5) Verificar se um ano é bissexto: Desenvolva um método que receba um ano
+		 * como argumento e retorne verdadeiro se o ano for bissexto e falso caso
+		 * contrário.
+		 * 
+		 */
 
 	public static boolean verificar(int ano) {
 		return (ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0);
 	}
 
 	public static void main(String[] args) {
-		/*
-		 * 5) Verificar se um ano é bissexto: Desenvolva um método que receba um ano
-		 * como argumento e retorne verdadeiro se o ano for bissexto e falso caso
-		 * contrário.
-		 * 
-		 */
+		
 		System.out.println("Digite um ano: ");
 		Scanner sc = new Scanner(System.in);	
 		int ano = sc.nextInt();
