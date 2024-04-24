c6f1e834d27debec379f17375283fddecc9f7d65
TALITA ANTUNES <127903101+Talitaav@users.noreply.github.com>
2024-04-24 19:54:47 -0300

    Update Exercicio07.java

--- tarefa13/Exercicio07.java
+++ tarefa13/Exercicio07.java
@@ -3,6 +3,12 @@ package tarefa13;
 import java.util.Scanner;
 
 public class Exercicio07 {
+	/*
+		 * 7) Verificar se uma string é palíndromo: Escreva um método que receba uma
+		 * string como argumento e retorne verdadeiro se a string for um palíndromo e
+		 * falso caso contrário.
+		 * 
+		 */
 
 	public static boolean palindromo(String texto) {
 
@@ -14,12 +20,7 @@ public class Exercicio07 {
 	}
 
 	public static void main(String[] args) {
-		/*
-		 * 7) Verificar se uma string é palíndromo: Escreva um método que receba uma
-		 * string como argumento e retorne verdadeiro se a string for um palíndromo e
-		 * falso caso contrário.
-		 * 
-		 */
+		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Digite uma frase: ");
 		String texto = sc.nextLine();
